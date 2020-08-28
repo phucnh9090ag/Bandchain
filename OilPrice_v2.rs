9d55f6f40450c9bf26b2269af33d87b1c89015df
nhattm <nhattm@lotusapp.vn>
2020-08-28 13:51:28 +0700

    update

--- OilPrice_v2.rs
+++ OilPrice_v2.rs
@@ -13,36 +13,14 @@ struct Output {
 
 #[no_mangle]
 fn prepare_impl(_input: Input) {
-    // Coingecko price data source
     oei::ask_external_data(1, 1, "".as_bytes());
 }
 
 #[no_mangle]
 fn execute_impl(input: Input) -> Output {
-    let median = ext::load_median::<f64>(1);
+    let avg: f64 = ext::load_average(1);
     Output {
-        price: (median.unwrap() * input.multiplier as f64) as u64,
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use obi::get_schema;
-    use std::collections::*;
-
-    #[test]
-    fn test_get_schema() {
-        let mut schema = HashMap::new();
-        Input::add_definitions_recursively(&mut schema);
-        Output::add_definitions_recursively(&mut schema);
-        let input_schema = get_schema(String::from("Input"), &schema);
-        let output_schema = get_schema(String::from("Output"), &schema);
-        println!("{}/{}", input_schema, output_schema);
-        assert_eq!(
-            "{multiplier:u64}/{price:u64}",
-            format!("{}/{}", input_schema, output_schema),
-        );
+        price: (avg * input.multiplier as f64) as u64,
     }
 }
 
