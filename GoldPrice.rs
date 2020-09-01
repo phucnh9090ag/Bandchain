b012d6e7b2253e9ff1cb2b17246be32a717b163d
nhattm <nhattm@lotusapp.vn>
2020-09-01 09:51:09 +0700

    update

--- GoldPrice.rs
+++ GoldPrice.rs
@@ -22,27 +22,5 @@ fn execute_impl(_: Input) -> Output {
     let majority = ext::load_majority::<u64>(1);
     Output { price: majority.unwrap() }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use obi::get_schema;
-    use std::collections::*;
-
-    #[test]
-    fn test_get_schema() {
-        let mut schema = HashMap::new();
-        Input::add_definitions_recursively(&mut schema);
-        Output::add_definitions_recursively(&mut schema);
-        let input_schema = get_schema(String::from("Input"), &schema);
-        let output_schema = get_schema(String::from("Output"), &schema);
-        println!("{}/{}", input_schema, output_schema);
-        assert_eq!(
-            "{multiplier:u64}/{price:u64}",
-            format!("{}/{}", input_schema, output_schema),
-        );
-    }
-}
-
 prepare_entry_point!(prepare_impl);
 execute_entry_point!(execute_impl);
